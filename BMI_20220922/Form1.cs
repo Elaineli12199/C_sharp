a1687ceeed0bb91c65d66f835084e82060a512fe
pokai-huang0828 <000429977@student.vcc.ca>
2022-09-22 23:57:53 +0800

    Added homework file

---
+++ BMI_20220922/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_20220922
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float Height,Weight;
+            Height = float.Parse(textBox1.Text);
+            Weight = float.Parse(textBox2.Text);
+
+            double BMI;
+            BMI = Weight / ((Height / 100)*(Height / 100));
+            label3.Text = "你的BMI值為" + BMI.ToString("F2");
+        }
+    }
+}
