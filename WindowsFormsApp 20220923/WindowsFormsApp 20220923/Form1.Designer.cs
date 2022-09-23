8a95868c597f5aa90c9b858f21ae0d0bfe54c76c
Elaineli12199 <syuan1219s@gmail.com>
2022-09-23 21:34:02 +0800

    lesson 3

---
+++ WindowsFormsApp 20220923/WindowsFormsApp 20220923/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp_20220923
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "輸入成績";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "成績在此";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "判斷位數";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "判斷等級";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "1~4選一個";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "1.豬  2.雞  3.牛  4.羊";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 206);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 301);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(193, 300);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "確認";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "1.成人票  2.雙人套票  3.敬老票或軍種票 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp 20220923/WindowsFormsApp 20220923/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_20220923
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // int score = int.Parse(textBox1.Text);
+            //if (score >= 60) label1.Text = "及格囉";
+            int score = int.Parse(textBox1.Text);
+
+            if (score >= 60)
+            {
+                label1.Text = "及格囉";
+                label1.ForeColor = Color.Blue;
+            } 
+            //括號是代表範圍 
+            //if後面若有好幾個句子 括號就用來讓電腦區分if與else句子的各自範圍
+            else
+            {
+                label1.Text = "去重考吧你哈哈";
+                label1.ForeColor = Color.Red;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int inputNum = int.Parse(textBox1.Text);
+            if (inputNum >= 1000)
+                label1.Text = "4位數以上";
+            else if (inputNum <= 9 && inputNum > 0 )
+                label1.Text = "個位數";
+            else if (inputNum <= 99 && inputNum > 0)
+                label1.Text = "2位數";
+            else if (inputNum <= 999 && inputNum > 0)
+                label1.Text = "3位數";
+
+            else
+                label1.Text = "負數";
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int inputNum = int.Parse(textBox1.Text);
+            if (inputNum >= 90)
+                label1.Text = "甲等";
+            else if (inputNum >= 80)
+                label1.Text = "乙等";
+            else if (inputNum >= 70)
+                label1.Text = "丙等";
+            else if (inputNum >= 60)
+                label1.Text = "丁等";
+            else
+                label1.Text = "戊";
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int choice = int.Parse(textBox2.Text);
+            string s = "";
+            switch (choice)
+            {
+                case 1:s = "豬排"; break;
+                case 2: s = "雞排"; break;
+                case 3: s = "牛排"; break;
+                case 4: s = "羊排"; break;
+                default:s = "不要給我亂選拉!";break;
+            }
+
+            label2.Text = s;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int choice = int.Parse(textBox3.Text);
+            string s = "";
+            switch (choice)
+            {
+                case 1: s = "成人票 $250"; break;
+                case 2: s = "雙人套票 $ 450"; break;
+                case 3: 
+                case 4: s = "軍種票或敬老票 $200"; break;
+                default: s = "不要給我亂選拉!"; break;
+            }
+
+            label3.Text = s;
+        }
+    }
+}
